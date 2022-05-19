5701dc7e751764b9d5bc92b0a57009abba0dd7e9
Yasunori Terao <y.terao765@gmail.com>
2022-05-19 09:23:33 +0000

    Add show_range_image

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -14,6 +14,7 @@
 import cv2
 import numpy as np
 import torch
+import zlib
 
 # add project directory to python path to enable relative imports
 import os
@@ -59,21 +60,38 @@ def show_range_image(frame, lidar_name):
     print("student task ID_S1_EX1")
 
     # step 1 : extract lidar data and range image for the roof-mounted lidar
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
+    if len(lidar.ri_return1.range_image_compressed) > 0:
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
     
     # step 2 : extract the range and the intensity channel from the range image
-    
+    ri_range = ri[..., 0]
+    ri_intensity = ri[..., 1]
+
     # step 3 : set values <0 to zero
+    ri_range[ri_range < 0] = 0.0
+    ri_intensity[ri_intensity < 0] = 0.0
     
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    r_min = np.min(ri_range)
+    r_max = np.max(ri_range)
+    ri_range = (ri_range-r_min) * 255/(r_max-r_min)
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    
+    intensity_1th = np.percentile(ri_intensity, 1)
+    intensity_99th = np.percentile(ri_intensity, 99)
+    ri_intensity[ri_intensity < intensity_1th] = intensity_1th
+    ri_intensity[ri_intensity > intensity_99th] = intensity_99th
+    ri_intensity = (ri_intensity-intensity_1th) * 255/(intensity_99th-intensity_1th)
+
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
+    img_range_intensity = np.vstack((ri_range, ri_intensity)).astype(np.uint8)
     
-    img_range_intensity = [] # remove after implementing all steps
     #######
-    ####### ID_S1_EX1 END #######     
-    
+    ####### ID_S1_EX1 END #######
+
     return img_range_intensity
 
 
